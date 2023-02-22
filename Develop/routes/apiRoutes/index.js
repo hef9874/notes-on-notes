b397b33533e383f40dedf8b324cf24402e33b48e
nah <hef9874@gmail.com>
2023-02-22 18:45:49 -0500

    add lib js file and degugging

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {createNewNote, updateDb} = require("../../lib/notes");
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 const {notes} = require("../../db/db.json");
 
 // show all notes in json data
@@ -10,7 +10,7 @@ router.get("/notes", (req, res) => {
   });
 
 router.post("/notes", (req, res) => {
-    req.body.id = uuidv4();
+    // req.body.id = uuidv4();
     const newNote = createNewNote(req.body, notes);
     res.json(newNote);
 });
--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT  || 3001;
 
-const apiRoutes = require('./routes/apiRoutes')
+const apiRoutes = require('../Develop/routes/apiRoutes/index')
+const htmlRoutes = require('../Develop/routes/apiRoutes/htmlRoutes/index')
+
+
 
 //ask express to create route for every file in the public folder and give it a route
 app.use(express.static('public'));
@@ -12,4 +15,10 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-//to route files
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API now on ${PORT}.`);
+});
